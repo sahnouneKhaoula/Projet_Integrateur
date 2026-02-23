adba51e5ca445795a08a087ad100423f898c7777
Gabriel Labrosse <gabriel_labrosse@hotmail.com>
2026-02-23 14:09:31 -0500

    feat: ajout sidebar navigation et layout principal

---
+++ src/Frontend/src/components/Espaces.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Espaces() {
+  return (
+    <div>
+      
+      <div className="Header"> Espaces </div>
+
+        <div className="Content">
+            <div className="div1"></div>
+            <div className="div2"></div>
+            <div className="div3"></div>
+        </div>
+    </div>
+  )
+}
